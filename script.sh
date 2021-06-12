e87a2377e7cb4c853867b06a7fc8e667490e7bf1
sikander <82051128+machine1337@users.noreply.github.com>
2021-06-12 15:14:31 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -16,11 +16,17 @@ CN='\033[1;38;5;247m'
 CNC='\033[1;38;5;051m'
 
 function bounty_recon(){
-echo -e ${CNC}"#######################################################"                                                    
-echo -e ${RED} "      Bug Bounty Recon Full Automation               #"
-echo -e ${CG}"        https://facebook.com/unknownclay              #"
-echo -e ${CP}"        Coded By:  Machine1337	                      #"
-echo -e ${CPO}"#######################################################"
+echo -e ${RED}"##################################################################"
+echo -e ${CP}"        ____  _            _      ____  _____           _   _    #"                                                  
+echo -e ${CP}"       | __ )| | __ _  ___| | __ |  _ \|___ /  ___ ___ | \ | |   #"                                                  
+echo -e ${CP}"       |  _ \| |/ _  |/ __| |/ / | |_) | |_ \ / __/ _ \|  \| |   #"                                                 
+echo -e ${CP}"       | |_) | | (_| | (__|   <  |  _ < ___) | (_| (_) | |\  |   #"                                               
+echo -e ${CP}"       |____/|_|\__ _|\___|_|\_\ |_| \_\____/ \___\___/|_| |_|   #"       
+echo -e ${CP}"              Automate Your Bug Bounty R3cOn                     #"                                           
+echo -e ${BLUE}"              https://facebook.com/unknownclay                   #"  
+echo -e ${YELLOW}"              Coded By: Machine404                               #"
+echo -e ${CG}"              https://github.com/machine1337                     #"
+echo -e ${RED}"################################################################## \n "
 }
 d=$(date +"%b-%d-%y %H:%M")
 
@@ -30,30 +36,30 @@ bounty_recon
 echo -n -e ${ORANGE}"\n[+] Enter Single domain (e.g evil.com) : " 
            read domain
 mkdir -p $domain $domain/vulnerabilities $domain/vulnerabilities/cors $domain/waybackurls $domain/target_wordlist $domain/gf $domain/vulnerabilities/openredirect/ $domain/vulnerabilities/xss_scan $domain/nuclei_scan $domain/vulnerabilities/LFI $domain/vulnerabilities/sqli
-echo -e "\n\e[00;34m################ Single Target Recon Started On: $d  ##################\e[00m"
+echo -e ${BLUE}"\n[+] Recon Started On $d: \n"
 sleep 1
-echo -e "\n\e[00;31m#################### checking services on domain ###########################\e[00m"
+echo -e ${CP}"[+] Checking Services On Target:- \n"
 echo "$domain" | httpx -threads 30 -o $domain/httpx.txt
 sleep 1
-echo -e "\n\e[00;32m#############Checking For Cors misconfiguration#####################\e[00m"
+echo -e ${GREEN}"\n[+] Searching For Cors Misconfiguration:- "
 python3 ~/tools/Corsy/corsy.py -i $domain/httpx.txt -t 15 | tee $domain/vulnerabilities/cors/cors_misconfig.txt
 sleep 1
-echo -e "\n\e[00;34m#############...Collecting URLS ...##########################\e[00m"
+echo -e ${CPO}"\n[+] Collecting URLS:- \n"
 cat $domain/httpx.txt | gau | tee $domain/waybackurls/tmp.txt
 cat $domain/waybackurls/tmp.txt | egrep -v "\.woff|\.ttf|\.svg|\.eot|\.png|\.jpep|\.jpeg|\.css|\.ico|\jpg" | sed 's/:80//g;s/:443//g' | sort -u >> $domain/waybackurls/wayback.txt
 
 rm $domain/waybackurls/tmp.txt
 sleep 1
-echo -e "\n\e[00;35m#############...Fuzz Faster You Fool Started  ...#####################\e[00m"
+echo -e ${CNC}"\n[+] FFUF Started On URLS:- "
 ffuf -c -u "FUZZ" -w $domain/waybackurls/wayback.txt -of csv -o $domain/waybackurls/valid-tmp.txt
 
 cat $domain/waybackurls/valid-tmp.txt | grep http | awk -F "," '{print $1}'  >>  $domain/waybackurls/valid.txt
 
 rm $domain/waybackurls/valid-tmp.txt
-echo -e "\n\e[00;36m##################Generating target based wordlist ###########################\e[00m"
+echo -e ${PINK}"\n[+] Generating Target Based Wordlist:- "
 cat $domain/waybackurls/wayback.txt | unfurl -unique paths > $domain/target_wordlist/paths.txt
 cat $domain/waybackurls/wayback.txt | unfurl -unique keys > $domain/target_wordlist/param.txt
-echo -e "\n\e[00;37m##################Gf Pattern Started ###########################\e[00m"
+echo -e ${BLUE}"\n[+] Gf Patterns Started on Valid URLS:- "
 gf xss $domain/waybackurls/valid.txt | tee $domain/gf/xss.txt
 gf ssrf $domain/waybackurls/valid.txt | tee $domain/gf/ssrf.txt
 gf sqli $domain/waybackurls/valid.txt | tee $domain/gf/sql.txt
@@ -63,23 +69,23 @@ gf aws-keys $domain/waybackurls/valid.txt | tee $domain/gf/awskeys.txt
 gf redirect $domain/waybackurls/valid.txt | tee $domain/gf/redirect.txt
 cat $domain/gf/redirect.txt | sed 's/\=.*/=/' | tee $domain/gf/purered.txt
 gf idor $domain/waybackurls/valid.txt | tee $domain/gf/idor.txt
-echo -e "\n\e[00;33m#############...Nuclei Scanner Started...#####################\e[00m"
+echo -e ${CP}"\n [+]Nuclei Scanner Started "
 cat $domain/httpx.txt | nuclei -t ~/tools/nuclei-templates/cves/ -c 50 -o $domain/nuclei_scan/cves.txt
 cat $domain/httpx.txt | nuclei -t ~/tools/nuclei-templates/vulnerabilities/ -c 50 -o $domain/nuclei_scan/vulnerabilities.txt
 cat $domain/httpx.txt | nuclei -t ~/tools/nuclei-templates/misconfiguration/ -c 50 -o $domain/nuclei_scan/misconfiguration.txt
 cat $domain/httpx.txt | nuclei -t ~/tools/nuclei-templates/technologies/ -c 50 -o $domain/nuclei_scan/tech.txt
-echo -e "\n\e[00;32m###############Searching For Open Redirect  ###########################\e[00m"
+echo -e ${ORANGE}"\n[+] Searching For Open Redirection "
 cat $domain/gf/redirect.txt | qsreplace FUZZ | tee $domain/vulnerabilities/openredirect/fuzzredirect.txt
 python3 ~/tools/OpenRedireX/openredirex.py -l $domain/vulnerabilities/openredirect/fuzzredirect.txt -p ~/tools/OpenRedireX/payloads.txt --keyword FUZZ | tee $domain/vulnerabilities/openredirect/confrimopenred.txt
-echo -e "\n\e[00;34m##################XSS Scanner Started ###########################\e[00m"
+echo -e ${GREEN}"\n[+] Searching For XSS"
 cat $domain/gf/xss.txt | kxss  | tee $domain/vulnerabilities/xss_scan/kxss.txt
 cat $domain/vulnerabilities/xss_scan/kxss.txt | awk '{print $9}' | sed 's/=.*/=/' | tee $domain/vulnerabilities/xss_scan/kxss1.txt
 cat $domain/vulnerabilities/xss_scan/kxss1.txt | dalfox pipe | tee $domain/vulnerabilities/xss_scan/dalfoxss.txt
 cat $domain/gf/xss.txt | grep "=" | qsreplace "'><sCriPt class=khan>prompt(1)</script>" | while read host do ; do curl --silent --path-as-is --insecure "$host" | grep -qs "'><sCriPt class=khan>prompt(1)" && echo "$host \033[0;31mVulnerable\n";done | tee $domain/vulnerabilities/xss_scan/vulnxss.txt
-echo -e "\n\e[00;35m#############...Searching For Sql Injection...#####################\e[00m"
+echo -e ${CG}"\n[+] Searching For SQL Injection"
 sqlmap -m $domain/gf/sql.txt --batch --random-agent --level 1 | tee $domain/vulnerabilities/sqli/sqlmap.txt
 sleep 1
-echo -e "\n\e[00;37m#############...Searching For LFI vulnerabilities...#####################\e[00m"
+echo -e ${BLUE}"\n[+] Searching For LFI VULN"
 cat $domain/gf/lfi.txt | qsreplace FUZZ | while read url ; do ffuf -u $url -mr "root:x" -w ~/tools/lfipayloads.txt -of csv -o $domain/vulnerabilities/LFI/lfi.txt -t 50 -c  ; done
 }
 
@@ -89,49 +95,49 @@ bounty_recon
 echo -n -e ${BLUE2}"\n[+] Full Recon with subdomains (e.g *.example.com): "
 read domain
 mkdir -p $domain $domain/domain_enum $domain/final_domains $domain/takeovers $domain/vulnerabilities $domain/vulnerabilities/xss_scan $domain/vulnerabilities/sqli $domain/vulnerabilities/cors  $domain/nuclei_scan $domain/waybackurls $domain/target_wordlist $domain/gf  $domain/vulnerabilities/LFI $domain/vulnerabilities/openredirect
-echo -e "\n\e[00;34m################ Full Recon Started On: $d  ##################\e[00m"
+echo -e ${RED}"\n[+] Massive Recon Started On $d:  "
 sleep 1
-echo -e "\n\e[00;31m#################### crt.sh Enumeration Started ###########################\e[00m"
+echo -e ${CPO}"\n[+] Crt.sh Enumeration Started:- "
 curl -s https://crt.sh/\?q\=%25.$domain\&output\=json | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u | tee $domain/domain_enum/crt.txt
-echo -e "\n\e[00;32m#################### subfinder Enumeration Started ###########################\e[00m"
+echo -e ${CP}"\n[+] subfinder Enumeration Started:- "
 subfinder -d $domain -o $domain/domain_enum/subfinder.txt
-echo -e "\n\e[00;33m#################### assetfinder Enumeration Started ###########################\e[00m"
+echo -e ${PINK}"\n[+] Assetfinder Enumeration Started:- "
 assetfinder -subs-only $domain | tee $domain/domain_enum/assetfinder.txt
-echo -e "\n\e[00;34m#################### Amass Enumeration Started ###########################\e[00m"
+echo -e ${ORANGE}"\n[+] Amass Enumeration Started:- "
 amass enum -passive -d $domain -o $domain/domain_enum/amass.txt
-echo -e "\n\e[00;35m#################### shuffledns  Started ###########################\e[00m"
+echo -e ${CN}"\n[+] Shuffledns Enumeration Started:- "
 shuffledns -d $domain -w /usr/share/seclists/Discovery/DNS/deepmagic.com-prefixes-top50000.txt -r ~/tools/resolvers/resolver.txt -o $domain/domain_enum/shuffledns.txt
-echo -e "\n\e[00;36m##################Collecting all subdomains into one file ###########################\e[00m"
+echo -e ${CP}"\n[+] Collecting All Subdomains Into Single File:- "
 cat $domain/domain_enum/*.txt > $domain/domain_enum/all.txt
-echo -e "\n\e[00;37m##################Resolving All Subdomains ###########################\e[00m"
+echo -e ${BLUE}"\n[+] Resolving All Subdomains:- "
 shuffledns -d $domain -list $domain/domain_enum/all.txt -o $domain/final_domains/domains.txt -r ~/tools/resolvers/resolver.txt
-echo -e "\n\e[00;30m##################Checking Services on subdomains ###########################\e[00m"
+echo -e ${PINK}"\n[+] Checking Services On Subdomains:- "
 cat $domain/final_domains/domains.txt | httpx -threads 30 -o $domain/final_domains/httpx.txt
-echo -e "\n\e[00;31m############Searching For Subdomain takeover Vulnerability#####################\e[00m"
+echo -e ${CP}"\n[+] Searching For Subdomain TakeOver:- "
 subzy -hide_fails -targets $domain/domain_enum/all.txt | tee $domain/takeovers/subzy.txt
 subjack -w $domain/domain_enum/all.txt -t 100 -timeout 30 -o $domain/takeovers/take.txt -ssl
-echo -e "\n\e[00;32m#############Checking For Cors misconfiguration#####################\e[00m"
+echo -e ${GREEN}"\n[+] Searching For Cors Misconfiguration:- "
 python3 ~/tools/Corsy/corsy.py -i $domain/final_domains/httpx.txt -t 15 | tee $domain/vulnerabilities/cors/cors_misconfig.txt
-echo -e "\n\e[00;33m#############...Nuclei Scanner Started...#####################\e[00m"
+echo -e ${CP}"\n[+] Nuclei Scanner Started:- "
 cat $domain/final_domains/httpx.txt | nuclei -t ~/tools/nuclei-templates/cves/ -c 50 -o $domain/nuclei_scan/cves.txt
 cat $domain/final_domains/httpx.txt | nuclei -t ~/tools/nuclei-templates/vulnerabilities/ -c 50 -o $domain/nuclei_scan/vulnerabilities.txt
 cat $domain/final_domains/httpx.txt | nuclei -t ~/tools/nuclei-templates/misconfiguration/ -c 50 -o $domain/nuclei_scan/misconfiguration.txt
 cat $domain/final_domains/httpx.txt | nuclei -t ~/tools/nuclei-templates/technologies/ -c 50 -o $domain/nuclei_scan/tech.txt
-echo -e "\n\e[00;34m#############...Collecting URLS ...#####################\e[00m"
+echo -e ${CPO}"\n[+] Collecting URLS:- "
 cat $domain/final_domains/domains.txt | gau | tee $domain/waybackurls/tmp.txt
 cat $domain/waybackurls/tmp.txt | egrep -v "\.woff|\.ttf|\.svg|\.eot|\.png|\.jpep|\.jpeg|\.css|\.ico|\jpg" | sed 's/:80//g;s/:443//g' | sort -u >> $domain/waybackurls/wayback.txt
 
 rm $domain/waybackurls/tmp.txt
-echo -e "\n\e[00;35m#############...Fuzz Faster You Fool Started  ...#####################\e[00m"
+echo -e ${CNC}"\n[+] FFUF Started On URLS:- "
 ffuf -c -u "FUZZ" -w $domain/waybackurls/wayback.txt -of csv -o $domain/waybackurls/valid-tmp.txt
 
 cat $domain/waybackurls/valid-tmp.txt | grep http | awk -F "," '{print $1}'  >>  $domain/waybackurls/valid.txt
 
 rm $domain/waybackurls/valid-tmp.txt
-echo -e "\n\e[00;36m##################Generating target based wordlist ###########################\e[00m"
+echo -e ${PINK}"\n[+] Generating Target Based Wordlist:- "
 cat $domain/waybackurls/wayback.txt | unfurl -unique paths > $domain/target_wordlist/paths.txt
 cat $domain/waybackurls/wayback.txt | unfurl -unique keys > $domain/target_wordlist/param.txt
-echo -e "\n\e[00;37m##################Gf Pattern Started ###########################\e[00m"
+echo -e ${BLUE}"\n[+] Gf Patterns Started on Valid URLS:- "
 gf xss $domain/waybackurls/valid.txt | tee $domain/gf/xss.txt
 gf ssrf $domain/waybackurls/valid.txt | tee $domain/gf/ssrf.txt
 gf sqli $domain/waybackurls/valid.txt | tee $domain/gf/sql.txt
@@ -141,18 +147,18 @@ gf aws-keys $domain/waybackurls/valid.txt | tee $domain/gf/awskeys.txt
 gf redirect $domain/waybackurls/valid.txt | tee $domain/gf/redirect.txt
 cat $domain/gf/redirect.txt | sed 's/\=.*/=/' | tee $domain/gf/purered.txt
 gf idor $domain/waybackurls/valid.txt | tee $domain/gf/idor.txt
-echo -e "\n\e[00;32m###############Searching For Open Redirect  ###########################\e[00m"
+echo -e ${ORANGE}"\n[+] Searching For Open Redirection:- "
 cat $domain/gf/redirect.txt | qsreplace FUZZ | tee $domain/vulnerabilities/openredirect/fuzzredirect.txt
 python3 ~/tools/OpenRedireX/openredirex.py -l $domain/vulnerabilities/openredirect/fuzzredirect.txt -p ~/tools/OpenRedireX/payloads.txt --keyword FUZZ | tee $domain/vulnerabilities/openredirect/confrimopenred.txt
-echo -e "\n\e[00;34m##################XSS Scanner Started ###########################\e[00m"
+echo -e ${GREEN}"\n[+] Searching For XSS:- "
 cat $domain/gf/xss.txt | kxss  | tee $domain/vulnerabilities/xss_scan/kxss.txt
 cat $domain/vulnerabilities/xss_scan/kxss.txt | awk '{print $9}' | sed 's/=.*/=/' | tee $domain/vulnerabilities/xss_scan/kxss1.txt
 cat $domain/vulnerabilities/xss_scan/kxss1.txt | dalfox pipe | tee $domain/vulnerabilities/xss_scan/dalfoxss.txt
 cat $domain/gf/xss.txt | grep "=" | qsreplace "'><sCriPt class=khan>prompt(1)</script>" | while read host do ; do curl --silent --path-as-is --insecure "$host" | grep -qs "'><sCriPt class=khan>prompt(1)" && echo "$host \033[0;31mVulnerable\n";done | tee $domain/vulnerabilities/xss_scan/vulnxss.txt
 
-echo -e "\n\e[00;35m#############...Searching For Sql Injection...#####################\e[00m"
+echo -e ${CG}"\n[+] Searching For SQL Injection:- "
 sqlmap -m $domain/gf/sql.txt --batch --random-agent --level 1 | tee $domain/vulnerabilities/sqli/sqlmap.txt
-echo -e "\n\e[00;37m#############...Searching For LFI vulnerabilities...#####################\e[00m"
+echo -e ${BLUE}"\n[+] Searching For LFI VULN:- "
 cat $domain/gf/lfi.txt | qsreplace FUZZ | while read url ; do ffuf -u $url -mr "root:x" -w ~/tools/lfipayloads.txt -of csv -o $domain/vulnerabilities/LFI/lfi.txt -t 50 -c  ; done
 }
 menu(){
